6984f5be18338149e21a23c0ff1035885cc79f45
Abhii <abhishekhsharma2002@gmail.com>
2025-05-31 14:13:44 +0530

    version chnages

--- test.ts
+++ test.ts
@@ -1,8 +1,6 @@
-const { getMetadata } = require("metadata-fetch");
+const { getMetadata } = require("metadata-fetch")
 
 const GetData = async () =>
-  await getMetadata("https://flowbite.com/docs/forms/search-input/").then(
-    (res) => console.log(res)
-  );
+  await getMetadata("https://metadata-fetcher.vercel.app/").then(res => console.log(res))
 
-GetData();
+GetData()
